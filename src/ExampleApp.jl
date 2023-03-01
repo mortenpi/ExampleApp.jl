494660cfb85fa51beb5a32b58ccf659ff9fc40c9
Morten Piibeleht <morten.piibeleht@juliahub.com>
2023-03-01 22:07:15 +1300

    Print arguments in more detail

--- src/ExampleApp.jl
+++ src/ExampleApp.jl
@@ -2,6 +2,13 @@ module ExampleApp
 
 function run(args)
     @info "Running ExampleApp" args
+    
+    for (i, arg) in enumerate(args)
+        @info "Argument $i" arg
+        if isfile(arg)
+            println(read(arg, String))
+        end
+    end
 end
 
 end
